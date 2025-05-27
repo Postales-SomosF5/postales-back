4f45ce069ac540fe7696ff63d1a7cc91d186ae91
carlasiles1 <carlasiles.19cs@gmail.com>
2025-05-27 09:42:57 +0200

    arreglo-init

--- app/__init__.py
+++ app/__init__.py
@@ -7,24 +7,25 @@ from app.routes.centro_routes import centros_bp
 from app.routes.sector_routes import sector_bp
 from app.routes.interes_routes import intereses_bp
 from app.routes.emparejamiento_routes import emparejamientos_bp
-from .extensions import db, bcrypt 
+from .extensions import db, bcrypt, mail
 import os
 from flask_jwt_extended import JWTManager
-from config import JWT_CONFIG, SECURITY_HEADERS, DB_CONFIG
+from config import JWT_CONFIG, SECURITY_HEADERS
 
-from app.extensions import db, bcrypt, mail
-
-mail.init_app(app)
 
 def create_app():
     load_dotenv()
     app = Flask(__name__)
     CORS(app)
 
+    # Configuración de base de datos
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY') or "secreto"
 
+    # Configuración de JWT
+    app.config['JWT_SECRET_KEY'] = JWT_CONFIG['JWT_SECRET_KEY']
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_CONFIG['JWT_ACCESS_TOKEN_EXPIRES']
+    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = JWT_CONFIG['JWT_REFRESH_TOKEN_EXPIRES']
 
     # Configuración Flask-Mail
     app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
@@ -34,32 +35,26 @@ def create_app():
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 
-
+    # Inicializar extensiones
     db.init_app(app)
     bcrypt.init_app(app)
-
-    app.register_blueprint(auth_bp, url_prefix='/api/auth')
-    
-    app.register_blueprint(centros_bp, url_prefix='/api')    
-    
-    app.register_blueprint(sector_bp, url_prefix='/api')    
-    
-    app.register_blueprint(intereses_bp, url_prefix='/api')    
-
-     # Configuración de JWT
-    app.config['JWT_SECRET_KEY'] = JWT_CONFIG['JWT_SECRET_KEY']
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_CONFIG['JWT_ACCESS_TOKEN_EXPIRES']
-    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = JWT_CONFIG['JWT_REFRESH_TOKEN_EXPIRES']
-    
-    # Inicializar extensiones
+    mail.init_app(app)
     jwt = JWTManager(app)
+
+    # Registrar blueprints
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(user_bp, url_prefix='/api')    
+    app.register_blueprint(centros_bp, url_prefix='/api')    
+    app.register_blueprint(sector_bp, url_prefix='/api')    
+    app.register_blueprint(intereses_bp, url_prefix='/api')    
+    app.register_blueprint(emparejamientos_bp, url_prefix='/api')
 
     # Ruta de prueba en la raíz
     @app.route('/')
     def index():
         return jsonify({"mensaje": "Bienvenidx a la API de Chamberos wuwhuho"}), 200
     
-    # Manejo de errores
+    # Seguridad en headers
     @app.after_request
     def add_security_headers(response):
         for header, value in SECURITY_HEADERS.items():
